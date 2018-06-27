490ae104419362288da93622cfb54bd5b3922d45
RoderikC <33225047+RoderikC@users.noreply.github.com>
2018-06-27 19:49:43 +0200

    first commit

---
+++ FajaLobiMVCE/FajaLobiMVCE/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FajaLobiMVCE.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BedNBreakfast()
+        {
+            return View();
+        }
+
+        public ActionResult Cafe()
+        {
+            return View();
+        }
+
+        public ActionResult Restaurant()
+        {
+            return View();
+        }
+
+        public ActionResult Bosproject()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
